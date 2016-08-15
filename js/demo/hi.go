26ca8eb74fabfbc38123db86c3f12e276f266acf
shaoying <shylinux@icloud.com>
2016-08-15 19:36:12 +0800

    vpn mod js/demo/hi.go add default page

--- js/demo/hi.go
+++ js/demo/hi.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"net/http"
 	"flag"
 	"fmt"
+	"net/http"
 )
 
 var (
-	path = flag.String("path", "./", "http server file path")
-	addr = flag.String("addr", ":9090", "http listen addr")
+	path  = flag.String("path", "./", "http server file path")
+	addr  = flag.String("addr", ":9090", "http listen addr")
+	index = flag.String("index", "hi.html", "http default page")
 )
 
 func main() {
@@ -17,8 +18,30 @@ func main() {
 		fmt.Printf("%s: %v\n", f.Name, f.Value)
 	})
 
-	http.Handle("/", http.FileServer(http.Dir(*path)))
+	http.HandleFunc("/", log(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, *index)
+		}
+
+		http.ServeFile(w, r, *path+r.URL.Path)
+	}))
+
 	http.ListenAndServe(*addr, nil)
+
 	println("listen error")
 }
 
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		println(r.RemoteAddr, r.Method, r.URL.Path)
+
+		h(w, r)
+
+		defer func() {
+			if e := recover(); e != nil {
+				println(e)
+			}
+		}()
+
+	}
+}
