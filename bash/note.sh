73f0ab3bd595fe054b127cb696bacea22280cb8b
shaoying <shylinux@icloud.com>
2016-09-08 18:07:00 +0800

    vpn add note.sh

---
+++ bash/note.sh
@@ -0,0 +1,54 @@
+man info
+
+# \ "" ''
+; && ||
+! ^ % &
+# | > >> << <
+./ .. ~ * ? [^]
+$ = 
+
+fc history alias script
+jobs bg fg kill wait nohup
+which whereis whatis appropos
+$EDITOR
+
+boot etc proc dev
+root mnt home var
+sbin lib bin usr
+
+file
+od
+
+ls cd pwd mkdir rmdir 
+cat head tail more less
+cp rm mv ln
+basename
+diff patch
+find grep sort
+
+iao
+hjkl web gftn
+cdy rsx zvm qup
+ernp
+wq
+mtd
+
+
+mknod
+sync
+chmod
+touch
+umask
+
+echo
+
+
+
+
+
+man sudo apt-get ps kill top free ping netstat ip who
+cat tee tail uniq sort tr cut paste grep sed awk perl python
+ls pwd cd ln cp rm mv chmod mkdir rmdir du find df mount umount tar
+ssh bash tmux git vim go svn nginx openssl sqlite3
+wget curl tcpdump lynx links
+gdb gcc make ar od nm ldd size file readelf
