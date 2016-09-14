9ded9c5a0fac4bafeca684ca6092f966053640c6
shaoying <shylinux@icloud.com>
2016-09-14 17:36:29 +0800

    vpn add go/src/demo

---
+++ go/src/demo/note.go
@@ -0,0 +1,55 @@
+     c
+   sh go
+  vi py js
+ARM Linux HTTP
+version help init
+add commit mv rm checkout
+log status diff grep reset
+tag branch merge rebase show
+clone remote fetch pull push
+
+$GOROOT $GOPATH $GOARCH $GOOS
+version help env tool run
+get list doc fmt fix vet
+build test install clean
+package import type const var func
+func init() {}
+func main() {}
+
+nil true false iota
+int int8 int16 int32 int64
+uint uint8 uint16 uint32 uint64
+float32 float64 complex64 complex128
+uintptr byte string rune error bool
+* [...] ... [] map[] chan
+struct{} interface{} func(){}
+
+, := , <-
+{} [] () . ->
+& * + - ! ^
+* / % + -
+<< >> & | ^
+> >= < <= != ==
+&& ||
+...
+
+complex() real() imag()
+new() make() delete() close()
+len() cap() append() copy()
+print() println() 
+
+if exp {} else {}
+for {break}
+for exp {continue}
+for exp; exp; exp {}
+for k, v := range l {}
+switch {case exp: fallthrough}
+switch exp {case exp: default}
+return goto defer recover() panic()
+go select {case <-:}
+
+os path time flag log runtime expvar sync
+io fmt bufio bytes net text html image mime
+unsafe syscall reflect errors testing debug
+math hash crypto sort container regexp database
+unicode strings strconv encoding archive compress
