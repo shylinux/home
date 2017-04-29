1eaa6c2b4c893ce20df087cbca2f252586568425
shaoying <shylinux@icloud.com>
2017-04-30 00:09:03 +0800

    mac add back.go

--- go/src/tool/back.go
+++ go/src/tool/back.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import ( // {{{
 	"crypto/md5"
 	"encoding/hex"
 	"flag"
@@ -8,60 +8,344 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path"
+	"time"
 )
 
-var (
-	srcPath = flag.String("src", "./", "source path")
-	dstPath = flag.String("dst", "./", "destination path")
-	dstHash = flag.String("hash", "hash", "destination path hash")
+// }}}
+var ( // {{{
+	ishelp          = flag.Bool("help", false, "usage: back [options] src dst")
+	istime          = flag.Bool("time", false, "compare files by time stamp")
+	islist          = flag.Bool("list", true, "list different(+ > < -) files")
+	issave          = flag.Bool("save", false, "save new(+) and newer(>) files from src to dst")
+	isload          = flag.Bool("load", false, "load new(-) and newer(<) files from dst to src")
+	issame          = flag.Bool("same", false, "save all(+ > <) from src to dst delete rest(-) from dst")
+	isbackup        = flag.Bool("backup", false, "backup all(+ > <) files from src to dst")
+	isrecover       = flag.Bool("recover", false, "recover all(- < >) files from dst to src")
+	isforce         = flag.Bool("force", false, "force copy with no confirm ")
+	maxbit    int64 = 1
+	maxsize   int64
+	sumsize   int64
+	allsize   int64
+	src       string
+	dst       string
 )
 
-func show(hash *os.File, base string, path string) {
-	err := os.Chdir(path)
-	if err != nil {
-		os.Exit(1)
+// }}}
+type Meta struct { // {{{
+	flag rune
+	size int64
+	hash string
+	name string
+	time time.Time
+}
+
+// }}}
+func sizes(s int64) string { // {{{
+	if s > 10000000000 {
+		return fmt.Sprintf("%dG", s/1000000000)
+	}
+	if s > 10000000 {
+		return fmt.Sprintf("%dM", s/1000000)
+	}
+	if s > 10000 {
+		return fmt.Sprintf("%dK", s/1000)
+	}
+	return fmt.Sprintf("%dB", s)
+}
+
+// }}}
+func save(size int64, src string, dst string) (err error) { // {{{
+	dir := path.Dir(dst)
+	if _, err = os.Stat(dir); err != nil {
+		if err = os.MkdirAll(dir, os.ModePerm); err != nil {
+			return err
+		}
+		fmt.Printf("create dst path %s\n", dir)
+	}
+repeat:
+	if !*isforce {
+		fmt.Printf("copy %s from %s to %s y(yes/no/quit/delete/compare):", sizes(size), src, dst)
+
+		var a string
+		fmt.Scanf("%s\n", &a)
+		if len(a) > 0 && a[0] == 'n' {
+			return nil
+		}
+		if len(a) > 0 && a[0] == 'q' {
+			os.Exit(0)
+		}
+		if len(a) > 0 && a[0] == 'd' {
+			return os.Remove(src)
+		}
+
+		if len(a) > 0 && a[0] == 'c' {
+			cmd := exec.Command("vim", "-d", src, dst)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			if cmd.Run() != nil {
+				fmt.Println("can find editor vim")
+			}
+			goto repeat
+		}
 	}
 
-	cwd, _ := os.Getwd()
-	if len(base) == 0 {
+	dstf, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstf.Close()
+
+	srcf, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcf.Close()
+
+	size, err = io.Copy(dstf, srcf)
+	sumsize += size
+	fmt.Printf("copy %s to %s ... done %%%d\n", sizes(size), dst, sumsize*100/allsize)
+	return nil
+}
+
+// }}}
+func back(srcmeta []*Meta, dstmeta []*Meta, action bool) error { // {{{
+	if *islist && !action {
+		fmt.Fprintf(os.Stdout, "\n  %*s %*s\n",
+			maxbit+12, "source info",
+			maxbit+12, "destination info")
+	}
+
+	for _, f := range srcmeta {
+		f.flag = '+'
+		var ff *Meta
+		for _, ff = range dstmeta {
+			if f.name == ff.name {
+				if (*istime && f.time.Equal(ff.time)) ||
+					(!*istime && f.hash == ff.hash) {
+					f.flag, ff.flag = '=', '='
+				} else if f.time.Before(ff.time) {
+					f.flag, ff.flag = '<', '<'
+				} else {
+					f.flag, ff.flag = '>', '>'
+				}
+				break
+			}
+		}
+
+		if !action {
+			allsize += f.size
+			if *islist {
+				if f.flag == '+' {
+					fmt.Fprintf(os.Stdout, "%c %*s %s %*s %11s  %s\n", f.flag,
+						maxbit, sizes(f.size), f.time.Format("01/02 15:04"),
+						maxbit, " ", " ", f.name)
+				} else if f.flag != '=' {
+					fmt.Fprintf(os.Stdout, "%c %*s %s %*s %11s  %s\n", f.flag,
+						maxbit, sizes(f.size), f.time.Format("01/02 15:04"),
+						maxbit, sizes(ff.size), ff.time.Format("01/02 15:04"),
+						f.name)
+				}
+			}
+			continue
+		}
+
+		size := f.size
+		srcf := path.Join(src, f.name)
+		dstf := path.Join(dst, f.name)
+		needcopy := false
+
+		switch f.flag {
+		case '>':
+			if *issave || *isbackup || *issame {
+				needcopy = true
+			}
+			if *isrecover {
+				srcf = path.Join(dst, f.name)
+				dstf = path.Join(src, f.name)
+				size = ff.size
+				needcopy = true
+			}
+		case '<':
+			if *isbackup || *issame {
+				needcopy = true
+			}
+			if *isload || *isrecover {
+				srcf = path.Join(dst, f.name)
+				dstf = path.Join(src, f.name)
+				size = ff.size
+				needcopy = true
+			}
+		case '+':
+			if *issave || *isbackup || *issame {
+				needcopy = true
+			}
+		}
+
+		if needcopy {
+			if err := save(size, srcf, dstf); err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, f := range dstmeta {
+		if f.flag != '-' {
+			continue
+		}
+
+		if action {
+			if *issame {
+				fmt.Printf("remove %s from %s y(yes/no):", f.name, dst)
+
+				var a string
+				fmt.Scanf("%s\n", &a)
+				if len(a) > 0 && a[0] == 'n' {
+					continue
+				}
+				if err := os.Remove(path.Join(dst, f.name)); err != nil {
+					return err
+				}
+			}
+			if *isload || *isrecover {
+				if err := save(f.size, path.Join(dst, f.name), path.Join(src, f.name)); err != nil {
+					return err
+				}
+			}
+		} else {
+			allsize += f.size
+			if *islist {
+				fmt.Fprintf(os.Stdout, "%c %*s %11s %*s %s  %s\n", f.flag,
+					maxbit, " ", " ",
+					maxbit, sizes(f.size), f.time.Format("01/02 15:04"),
+					f.name)
+			}
+		}
+	}
+
+	return nil
+}
+
+// }}}
+func show(meta []*Meta, file string, base string) ([]*Meta, error) { // {{{
+
+	err := os.Chdir(file)
+	if err != nil {
+		return nil, err
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	if base == "" {
 		base = cwd
 		cwd = ""
 	} else {
 		cwd = cwd[len(base)+1:] + "/"
 	}
 
-	list, _ := ioutil.ReadDir("./")
+	list, err := ioutil.ReadDir("./")
+	if err != nil {
+		return nil, err
+	}
+
 	for _, v := range list {
 		if v.IsDir() {
 			if v.Name() != "." && v.Name() != ".." {
-				show(hash, base, v.Name())
+				if meta, err = show(meta, v.Name(), base); err != nil {
+					return nil, err
+				}
 			}
 			continue
 		}
 
-		f, _ := os.Open(v.Name())
-		h := md5.New()
-		io.Copy(h, f)
-		b := h.Sum(nil)
-		fmt.Fprintf(hash, "%d %s %s%s\n", v.ModTime().Unix(), hex.EncodeToString(b), cwd, v.Name())
+		f, err := os.Open(v.Name())
+		if err != nil {
+			return nil, err
+		}
 
+		m := new(Meta)
+		m.size = v.Size()
+		allsize += m.size
+		if m.size > maxsize {
+			maxsize = m.size
+		}
+
+		if !*istime {
+			h := md5.New()
+			if _, err := io.Copy(h, f); err != nil {
+				f.Close()
+				return nil, err
+			}
+			b := h.Sum(nil)
+			fmt.Print(".")
+			m.hash = hex.EncodeToString(b)
+		}
+
+		m.flag = '-'
+		m.time = v.ModTime()
+		m.name = fmt.Sprintf("%s%s", cwd, v.Name())
+		meta = append(meta, m)
+		f.Close()
 	}
 
-	err = os.Chdir("../")
+	if err = os.Chdir("../"); err != nil {
+		return nil, err
+	}
+
+	return meta, nil
+}
+
+// }}}
+func err_exit(err error, format string, str ...interface{}) { // {{{
 	if err != nil {
+		fmt.Printf(format, str)
 		os.Exit(1)
 	}
 }
 
-func main() {
-	flag.Parse()
-	flag.VisitAll(func(f *flag.Flag) {
-		fmt.Printf("%s %v\n", f.Name, f.Value)
-	})
+// }}}
+func main() { // {{{
+	if flag.Parse(); flag.NArg() != 2 {
+		fmt.Printf("usage %s [options] src dst\n", os.Args[0])
+		os.Exit(1)
+	}
 
-	f, _ := os.Create(*dstPath + *dstHash)
-	show(f, "", *srcPath)
-	f.Close()
+	src = path.Clean(flag.Arg(0))
+	_, err := os.Stat(src)
+	err_exit(err, "src path %s doesn't exist\n", src)
 
-	println("hello go world!")
+	dst = path.Clean(flag.Arg(1))
+	if _, err := os.Stat(dst); err != nil {
+		fmt.Printf("dst path %s doesn't exist\n", dst)
+		err = os.MkdirAll(dst, os.ModePerm)
+		err_exit(err, "%s", err)
+		fmt.Printf("create dst path %s\n", dst)
+	}
+
+	allsize = 0
+	fmt.Printf("sum src: ")
+	srcmeta, err := show(make([]*Meta, 0), src, "")
+	err_exit(err, "%s", err)
+	fmt.Printf(" %d files %s bytes\n", len(srcmeta), sizes(allsize))
+
+	allsize = 0
+	fmt.Printf("sum dst: ")
+	dstmeta, err := show(make([]*Meta, 0), dst, "")
+	err_exit(err, "%s", err)
+	fmt.Printf(" %d files %s bytes\n", len(dstmeta), sizes(allsize))
+
+	for maxsize > 0 {
+		maxbit++
+		maxsize /= 10
+	}
+
+	allsize = 0
+	back(srcmeta, dstmeta, false)
+	back(srcmeta, dstmeta, true)
 }
+
+// }}}
